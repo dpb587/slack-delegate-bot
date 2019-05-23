3f627b2561cc95cfb00bf5ffc9fe571db749956e
Danny Berger <dpb587@gmail.com>
2019-05-23 09:04:32 -0700

    Update go-pairist to fix long-running auth

--- vendor/github.com/dpb587/go-pairist/api/auth.go
+++ vendor/github.com/dpb587/go-pairist/api/auth.go
@@ -2,8 +2,13 @@ package api
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"strings"
+	"time"
 
+	"github.com/pkg/errors"
 	identitytoolkit "google.golang.org/api/identitytoolkit/v3"
 	"google.golang.org/api/option"
 )
@@ -13,11 +18,12 @@ type Auth struct {
 	Team     string
 	Password string
 
-	idToken string
+	idToken      string
+	refreshAfter time.Time
 }
 
 func (a *Auth) IDToken() (string, error) {
-	if a.idToken == "" {
+	if a.idToken == "" || a.refreshAfter.Before(time.Now()) {
 		ctx := context.Background()
 
 		client, err := identitytoolkit.NewService(ctx, option.WithAPIKey(a.APIKey))
@@ -25,7 +31,6 @@ func (a *Auth) IDToken() (string, error) {
 			return "", err
 		}
 
-		// seems like these tokens do not expire, so no need to refresh?
 		resp, err := client.Relyingparty.VerifyPassword(&identitytoolkit.IdentitytoolkitRelyingpartyVerifyPasswordRequest{
 			Email:             fmt.Sprintf("%s@pair.ist", a.Team),
 			Password:          a.Password,
@@ -36,6 +41,27 @@ func (a *Auth) IDToken() (string, error) {
 		}
 
 		a.idToken = resp.IdToken
+
+		idTokenSplit := strings.SplitN(a.idToken, ".", 3)
+		if len(idTokenSplit) != 3 {
+			return "", fmt.Errorf("parsing auth token: expected 3 parts, found %d", len(idTokenSplit))
+		}
+
+		claimsData, err := base64.StdEncoding.DecodeString(idTokenSplit[1])
+		if err != nil {
+			return "", errors.Wrap(err, "decoding auth token")
+		}
+
+		var claims struct {
+			Exp int64 `json:"exp"`
+		}
+
+		err = json.Unmarshal(claimsData, &claims)
+		if err != nil {
+			return "", errors.Wrap(err, "unmarshalling auth token claims")
+		}
+
+		a.refreshAfter = time.Unix(claims.Exp, 0).Add(-5 * time.Minute)
 	}
 
 	return a.idToken, nil
